adec1f030e47d56d0417b0e777eff14800358d70
Salas <94945634+SalasJ13@users.noreply.github.com>
2021-12-20 12:26:48 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let name="Jazmin";
+let lastname="Salas";
+let school="ITT";
+let city="Tijuana";
+let country="Mexico";
+let color="Black";
+let carrera="ISC";
+let food="Spaguetti";
+let food1="Lasagna";
+let plant="Roses";
+let song="So happy";
+let icecream="Chocolate";
+let github="SalasJ13"
+let email="salasjazmin15@gmail.com";
+let laptop="HP";
+let portafo="Jazmin Salas"
+let lenguaje="Espanol";
+let program="C#";
+let program1="C++";
+let bd="SQL server";
+
+
+document.write(`My name is <b>${name}</b> <b>${lastname}</b>, I study at the <b>${school}</b> university in <b>${city}</b> <b>${country}</b> in the <b>${carrera}</b>
+career, the programming languages are <b>${program}</b> and <b>${program1}</b>, I make a database in <b>${bd}</b>,
+my email is <b>${email}</b>, my github account It's <b>${github}</b>, I also have a Portfolium <b>${portafo}</b>.
+My favorite foods are <b>${food}</b> and <b>${food1}</b>. I like roses and <b>${color}</b>and my favorite song is <b>${song}</b>.
+I work on an <b>${laptop}</b> laptop and speak <b>${lenguaje}</b>.`);
